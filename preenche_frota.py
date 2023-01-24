6b72596257cf8395a72ed69e3ce7ae64fab65081
Francesco Rodrigues <keko.vaccario@gmail.com>
2023-01-24 11:51:43 -0300

    preenche frota

---
+++ preenche_frota.py
@@ -0,0 +1,19 @@
+
+def define_posicoes(linha, coluna, orientacao, tamanho):
+    posicoes = []
+    if orientacao == "vertical":
+        for i in range(tamanho):
+            posicoes.append([linha + i, coluna])
+    else:
+        for i in range(tamanho):
+            posicoes.append([linha, coluna + i])
+    return posicoes
+    
+
+def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
+    posicoes = define_posicoes(linha, coluna, orientacao, tamanho)
+    if nome_navio not in frota:
+        frota[nome_navio] = [posicoes]
+    else:
+        frota[nome_navio].append(posicoes)
+    return frota
