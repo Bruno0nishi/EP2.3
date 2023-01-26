49852f4872a402759b1b20faf9c8c553308a7de1
Bruno0nishi <brunoo2@al.insper.edu.br>
2023-01-25 22:13:18 -0300

    posiciona frota

---
+++ posiciona_frota.py
@@ -0,0 +1,9 @@
+def posiciona_frota(frota):
+    tabuleiro = [[0]*10 for _ in range(10)]
+    for tipo_navio, posicoes in frota.items():
+        for posicao in posicoes:
+            for x, y in posicao:
+                tabuleiro[x][y] = 1
+    return tabuleiro
+
+    #cria um tabuleiro de 10x10 usando uma lista
